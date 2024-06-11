1ee3de4149dab91713a307b36262ed537c6a1266
Tenny <tenny.shu@foxmail.com>
2024-06-11 17:01:39 +0800

    feat: 重新优化配置

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
 function kebabCase(key) {
-	const result = key.replace(/([A-Z])/g, " $1").trim();
-	return result.split(" ").join("-").toLowerCase();
+  const result = key.replace(/([A-Z])/g, " $1").trim();
+  return result.split(" ").join("-").toLowerCase();
 }
 
 export default function neatuiResolver() {
-	return [
-		{
-			type: "component",
-			resolve: (name) => {
-				if (name.startsWith("Nt")) {
-					const componentName = name.slice(2);
-					const styleName = kebabCase(componentName);
-					return {
-						name: componentName,
-						from: "@asteres/neatui-vue",
-						sideEffects: `@asteres/neatui-vue/style/${styleName}/index.js`,
-					};
-				}
-			},
-		},
-		{
-			type: "directive",
-			resolve: (name) => {
-				const directives = {
-					Clickoutside: {
-						importName: "Clickoutside",
-						styleName: undefined,
-					},
-				};
-				if (Object.hasOwn(directives, name)) {
-					return {
-						name: directives[name].importName,
-						from: "@asteres/neatui-vue",
-						sideEffects: directives[name].styleName,
-					};
-				}
-			},
-		},
-	];
+  return [
+    {
+      type: "component",
+      resolve: (name) => {
+        if (name.startsWith("Nt")) {
+          const componentName = name.slice(2);
+          const styleName = kebabCase(componentName);
+          let effectStyle = `@asteres/neatui-vue/style/${styleName}/index.js`;
+          if (componentName.endsWith("Icon")) {
+            effectStyle = "@asteres/neatui-vue/style/icon/index.js";
+          }
+          return {
+            name: componentName,
+            from: "@asteres/neatui-vue",
+            sideEffects: effectStyle,
+          };
+        }
+      },
+    },
+    {
+      type: "directive",
+      resolve: (name) => {
+        const directives = {
+          Clickoutside: {
+            importName: "Clickoutside",
+            styleName: undefined,
+          },
+        };
+        if (Object.hasOwn(directives, name)) {
+          return {
+            name: directives[name].importName,
+            from: "@asteres/neatui-vue",
+            sideEffects: directives[name].styleName,
+          };
+        }
+      },
+    },
+  ];
 }
